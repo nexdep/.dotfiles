622bb765a209fe0c299efd425ccff3b39bcacb32
marco-de-pietri <marco.deppo@hotmail.it>
2025-09-15 18:15:20 -0400

    nvim stuff

--- nvim/.config/nvim/lua/plugins/autosave.lua
+++ nvim/.config/nvim/lua/plugins/autosave.lua
@@ -1,10 +1,30 @@
 return {
   "okuuva/auto-save.nvim",
-  version = "*", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
-  cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+  event = { "InsertLeave", "TextChanged" }, -- lazy-load on first likely triggers
+  cmd = "ASToggle",
+  keys = {
+    { "<leader>ua", "<cmd>ASToggle<cr>", desc = "Toggle Autosave" },
+  },
   opts = {
-    -- your config goes here
-    -- or just leave it empty :)
+    debounce_delay = 1000, -- 1s: gentle on watchers/formatters
+    trigger_events = {
+      immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" },
+      defer_save = { "InsertLeave", "TextChanged" },
+      cancel_deferred_save = { "InsertEnter" },
+    },
+    -- Skip special/ephemeral buffers & UIs
+    condition = function(buf)
+      local bt = vim.bo[buf].buftype
+      local ft = vim.bo[buf].filetype
+      if bt ~= "" then return false end
+      local excluded = {
+        "gitcommit", "TelescopePrompt", "neo-tree", "lazy", "lazygit",
+        "oil", "toggleterm", "alpha", "dashboard", "Outline", "prompt",
+      }
+      return not vim.tbl_contains(excluded, ft)
+    end,
+    -- Avoid extra autocmd noise when a formatter also runs on save:
+    noautocmd = false,
   },
 }
+
