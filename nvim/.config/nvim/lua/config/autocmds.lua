bfb124a971e30796402dd5ddf8677c0f3fba5230
marco-de-pietri <marco.deppo@hotmail.it>
2026-01-13 17:41:33 -0500

    handy terminal movements in lazyvim

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,33 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Terminal buffer-only keymaps:
+--   Terminal-mode: <C-k> -> Terminal-Normal mode
+--   Terminal-Normal mode: <C-j> -> back to Terminal-mode (insert)
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function(ev)
+    local opts = { buffer = ev.buf, silent = true, noremap = true }
+
+    -- When you're typing in the terminal (Terminal-mode), go to normal mode
+    vim.keymap.set(
+      "t",
+      "<C-k>",
+      [[<C-\><C-n>]],
+      vim.tbl_extend("force", opts, {
+        desc = "Terminal: to normal mode",
+      })
+    )
+
+    -- When you're in normal mode inside the terminal buffer, go back to typing
+    vim.keymap.set(
+      "n",
+      "<C-j>",
+      "i",
+      vim.tbl_extend("force", opts, {
+        desc = "Terminal: to terminal mode",
+      })
+    )
+  end,
+})
