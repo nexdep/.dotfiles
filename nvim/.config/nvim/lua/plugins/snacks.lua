ebf09ce33abfc5ff94f650b7d1a41450df3515fb
marco-de-pietri <marco.deppo@hotmail.it>
2026-02-25 18:05:55 -0500

    test

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,20 +1,19 @@
 return {
-	{
-		"folke/snacks.nvim",
-		opts = {
-			words = {
-				enabled = true,
-			        },
-			picker = {
-				hidden = true,   -- show files starting with “.”
-				ignored = true,  -- include files matched by .gitignore
-				sources = {
-					  files = {
-						    hidden  = true,
-						    ignored = true,
-						  },
-					},
-			      },
-			},
-	},
+  {
+    "folke/snacks.nvim",
+    opts = {
+      words = {
+        enabled = true,
+      },
+      picker = {
+        hidden = true, -- show files starting with “.”
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
 }
