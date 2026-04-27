eb8d9a261cb632d37b468f1dcdce834c62f8b6bd
nexdep <marco.depietri90@gmail.com>
2026-04-27 12:07:01 -0400

    gh install script

--- scripts/ghsetup_wsl_ubuntu.sh
+++ scripts/ghsetup_wsl_ubuntu.sh
@@ -33,7 +33,7 @@ apt-get autoremove -y
 apt-get autoclean -y
 
 # Install basic packages
-apt-get -y install gcc stow zsh curl p7zip build-essential software-properties-common unzip
+apt-get -y install gcc stow zsh curl p7zip build-essential software-properties-common unzip wget
 
 # install node and npm
 cd "$SCRIPT_HOME"
@@ -47,6 +47,17 @@ npm install -g tree-sitter-cli
 npm install -g @openai/codex
 npm install -g @bitwarden/cli
 EOF
+
+# install gh cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+	&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+	&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
 
 # Install more packages
 apt-get -y install ffmpeg pandoc fd-find ripgrep tmux
